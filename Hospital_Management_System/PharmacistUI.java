8de97179bdeee2a555df0bd490e1c0d99e1d8dec
AuroraVane <47216870+AuroraVane@users.noreply.github.com>
2024-10-25 15:29:39 +0800

    Spelling Error

--- Hospital_Management_System/PharmacistUI.java
+++ Hospital_Management_System/PharmacistUI.java
@@ -9,7 +9,7 @@ public class PharmacistUI implements UserUI{
     }
     public void printMenu(){
         System.out.println("1. View Appointment Outcome Record");
-        System.out.println("2. Update Presccription Status");
+        System.out.println("2. Update Prescription Status");
         System.out.println("3. View Medication Inventory");
         System.out.println("4. Submit Replenishment Request");
         System.out.println("5. Log Out");
