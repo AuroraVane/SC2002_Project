7a2fe80b5cd61526a7f644044fce3fd5f912aad7
qwertyaditi <av09.hw@gmail.com>
2024-10-25 14:10:13 +0800

    Update Patient.java

--- Hospital_Management_System/Patient.java
+++ Hospital_Management_System/Patient.java
@@ -1,22 +1,39 @@
-public class Patient extends User{
-    private String DOB;
-    private String bloodtype;
-    private String contactinfo;
 
+public class Patient extends User {
+    private String DOB;
+    private String bloodType;
+    private String contactInfo;
+    private MedicalRecord medicalRecord;
     private PatientUI patientUI;
-    
-    public Patient(String id, String name, String password, String gender,String DOB, String bloodtype, String contactinfo) {
-        super(id, name, password,gender);
+
+    public Patient(String id, String name, String password, String gender, 
+                   String DOB, String bloodType, String contactInfo) {
+        super(id, name, password, gender);
         this.DOB = DOB;
-        this.bloodtype = bloodtype;
-        this.contactinfo = contactinfo;
+        this.bloodType = bloodType;
+        this.contactInfo = contactInfo;
+        this.medicalRecord = new MedicalRecord();
         this.patientUI = new PatientUI(this);
     }
+
     @Override
     public String getRole() {
         return "Patient";
     }
-    public void displayUI(){
+
+    public void displayUI() {
         this.patientUI.printMenu();
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public MedicalRecord getMedicalRecord() {
+        return medicalRecord;
     }
 }
