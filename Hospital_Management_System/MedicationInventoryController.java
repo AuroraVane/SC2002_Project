2189e9053e8a7d4710532922db73d5b0f0355736
AuroraVane <47216870+AuroraVane@users.noreply.github.com>
2024-10-25 23:50:49 +0800

    Fixed MedicineController

--- Hospital_Management_System/MedicationInventoryController.java
+++ Hospital_Management_System/MedicationInventoryController.java
@@ -61,6 +61,12 @@ public class MedicationInventoryController {
 
         this.writer = new TextFileWriter();
         writer.updateMedicationInventory(name, stock);
+        try{
+            medicationInventoryList = TextFileReader.loadMedicationInventory("Medicine_List.txt");
+        } catch (IOException e) {
+            System.out.println("Error: Unable to load medication inventory list from file.");
+            e.printStackTrace(); // Optional: To print the stack trace for debugging
+        }
     }
     
     public void updateMedicationInventory(String name,String stock){
