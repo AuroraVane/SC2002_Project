0c2e1dcd9bfa674a0778e98030ad89a7ec938cc0
qwertyaditi <av09.hw@gmail.com>
2024-10-29 13:49:46 +0800

    Update MedicalRecord.java

--- Hospital_Management_System/MedicalRecord.java
+++ Hospital_Management_System/MedicalRecord.java
@@ -1,33 +1,48 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class MedicalRecord {
-    private List<String> diagnoses;
-    private List<String> treatments;
+    private Patient patient;
+    private ArrayList<String> diagnoses;
+    private ArrayList<String> treatments;
 
-    public MedicalRecord() {
+    public MedicalRecord(Patient patient) {
+        this.patient = patient;
         this.diagnoses = new ArrayList<>();
         this.treatments = new ArrayList<>();
     }
 
+    public void viewMedicalRecord() {
+        System.out.println("Medical Record for " + patient.getName() + ":");
+        System.out.println("DOB: " + patient.getDOB());
+        System.out.println("Blood Type: " + patient.getBloodType());
+        System.out.println("Diagnoses: ");
+        for (String diagnosis : diagnoses) {
+            System.out.println("- " + diagnosis);
+        }
+        System.out.println("Treatments: ");
+        for (String treatment : treatments) {
+            System.out.println("- " + treatment);
+        }
+    }
+
+    // Adding a new diagnosis to the medical record
     public void addDiagnosis(String diagnosis) {
         diagnoses.add(diagnosis);
+        System.out.println("New diagnosis added: " + diagnosis);
     }
 
+    // Adding a new treatment to the medical record
     public void addTreatment(String treatment) {
         treatments.add(treatment);
+        System.out.println("New treatment added: " + treatment);
     }
 
-    public List<String> getDiagnoses() {
+    // Getters
+    public ArrayList<String> getDiagnoses() {
         return diagnoses;
     }
 
-    public List<String> getTreatments() {
+    public ArrayList<String> getTreatments() {
         return treatments;
-    }
-
-    @Override
-    public String toString() {
-        return "Diagnoses: " + diagnoses + "\nTreatments: " + treatments;
     }
 }
