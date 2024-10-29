8d3c95fd0059ce87a49f6ce081bcf5b4abce15a4
qwertyaditi <av09.hw@gmail.com>
2024-10-29 13:50:08 +0800

    Update Patient.java

--- Hospital_Management_System/Patient.java
+++ Hospital_Management_System/Patient.java
@@ -1,19 +1,34 @@
-
 public class Patient extends User {
     private String DOB;
     private String bloodType;
     private String contactInfo;
     private MedicalRecord medicalRecord;
-    private PatientUI patientUI;
+    private AppointmentController appointmentController;
 
-    public Patient(String id, String name, String password, String gender, 
-                   String DOB, String bloodType, String contactInfo) {
+    public Patient(String id, String name, String password, String gender, String DOB, String bloodType, String contactInfo) {
         super(id, name, password, gender);
         this.DOB = DOB;
         this.bloodType = bloodType;
         this.contactInfo = contactInfo;
-        this.medicalRecord = new MedicalRecord();
-        this.patientUI = new PatientUI(this);
+        this.medicalRecord = new MedicalRecord(this);
+        this.appointmentController = new AppointmentController();
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String newContactInfo) {
+        this.contactInfo = newContactInfo;
+        System.out.println("Contact information updated successfully.");
     }
 
     @Override
@@ -21,19 +36,16 @@ public class Patient extends User {
         return "Patient";
     }
 
-    public void displayUI() {
-        this.patientUI.printMenu();
-    }
-
-    public String getContactInfo() {
-        return contactInfo;
-    }
-
-    public void setContactInfo(String contactInfo) {
-        this.contactInfo = contactInfo;
-    }
-
     public MedicalRecord getMedicalRecord() {
         return medicalRecord;
+    }
+
+    public AppointmentController getAppointmentController() {
+        return appointmentController;
+    }
+
+    public void displayUI() {
+        PatientUI patientUI = new PatientUI(this);
+        patientUI.printMenu();
     }
 }
