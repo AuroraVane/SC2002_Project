3ccc28c3186bd5c3efc0e9efd69f884f182c84cd
huegashi <msnaive16@gmail.com>
2024-11-10 16:40:31 +0800

    spacing issue fix

--- Hospital_Management_System/boundary/PatientMedicalRecordUI.java
+++ Hospital_Management_System/boundary/PatientMedicalRecordUI.java
@@ -1,9 +1,8 @@
 package boundary;
 
+import entity.Patient;
 import java.io.IOException;
 import java.util.List;
-
-import entity.Patient;
 import utils.TextFileReader;
 
 public class PatientMedicalRecordUI {
@@ -17,16 +16,18 @@ public class PatientMedicalRecordUI {
     public void viewMedicalRecord(Patient p) {
         System.out.println("Medical Record for " + p.getName() + ":");
         System.out.println("DOB: " + p.getDOB());
-        System.out.println("Blood Type: " + p.getBloodtype());
+        System.out.println("Blood Type: " + p.getBloodtype()+"\n");
         try {
             List<MedicalRecord> mRecords = TextFileReader.loadMedicalRecord("./TextFiles/MedicalRecord.txt", "./TextFiles/Patient_List.txt");
             for (MedicalRecord x : mRecords){
                 if(p.getId().equals(x.getPatient().getId())){
+                
                     System.out.println("Diagnosis History:");
                     TextFileReader.NormalRead("./TextFiles/"+x.getDiagnosis_FILEPATH());
-                
+                    System.out.println("\n");
                     System.out.println("Treatment Plans:");
                     TextFileReader.NormalRead("./TextFiles/"+x.getTreatment_plans_FILEPATH());
+                    System.out.println("\n");
                 }
             }
         } catch (IOException e) {
